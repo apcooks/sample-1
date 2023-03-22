537186c79089773dbe46834097163308b3c60524
paparn95 <128636404+paparn95@users.noreply.github.com>
2023-03-22 15:42:42 -0400

    task0222032023342

---
+++ idle1.py
@@ -0,0 +1,38 @@
+def fun1():
+    print ("function with out arguments and with out return type")
+    a=5
+    b=2
+    c=a+b
+    print("sum = ",c)
+    
+fun1()
+
+def fun2(x,y):
+    print("Function with arguments and with out return type")
+    a=x
+    b=y
+    c=a+b
+    print("sum = ",c)
+
+fun2(20,10)
+
+def fun3():
+    print("Function with out arguments and with return type")
+    a=22
+    b=4
+    c=a+b
+    return c
+print("sum = ",fun3())
+
+
+def fun4(x,y):
+    print("Function with arguments and with out return type")
+    a=x
+    b=y
+    c=a+b
+    return c
+print("sum = ",fun4(15,4))
+
+
+          
+                
